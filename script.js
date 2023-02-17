4e929f6ebb4e59bd6d1be17e078485a7efeb7e19
Chandan8585 <chandanwingshr@gmail.com>
2023-02-17 21:58:51 +0530

    stuck at building logic

---
+++ script.js
@@ -0,0 +1,10 @@
+const twoPlayer = document.querySelector(".two-player");
+const computer = document.querySelector(".computer");
+const reset = document.querySelector(".reset");
+
+computer.addEventListener("click", function(){
+         getComputerChoice()
+
+
+
+} )
